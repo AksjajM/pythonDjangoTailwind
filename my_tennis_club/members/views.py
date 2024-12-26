4426217fcf566b64cf5ea5cae42901293a20dbfc
AksjajM <22161244@student.hhs.nl>
2024-12-26 16:59:36 +0100

    added a details page that shows info of an individual member

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -11,3 +11,12 @@ def members(request):
         'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    # retrieve the information of the individual member
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
